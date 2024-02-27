65b661e8815056898d2ccbebe041b2496049e915
Mariam <kelanimaryam1234@gmail.com>
2024-02-27 07:31:04 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,13 +9,18 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_add;
+
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
-	binary_tree_t *new_add = binary_tree_node(parent, value);
-
+	new_add = binary_tree_node(parent, value);
 	if (new_add == NULL)
+	{
 		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
@@ -23,7 +28,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_add;
 	}
 	parent->left = new_add;
-
 	return (new_add);
 }
 
