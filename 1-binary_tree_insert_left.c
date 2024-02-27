63271a3544b499a6a54bc608032fbca977d59e11
Mariam <kelanimaryam1234@gmail.com>
2024-02-27 06:57:59 -0800

    1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts left-child node of another node
+ * @parent: the parent in the binary tree
+ * @value: the value to be used
+ *
+ * Return: the new memory addrress in the binary tree
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	binary_tree_t *new_add = binary_tree_node(parent, value);
+
+	if (new_add == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new_add->left = parent->left;
+		parent->left->parent = new_add;
+	}
+	parent->left = new_add;
+
+	return (new_add);
+}
+
