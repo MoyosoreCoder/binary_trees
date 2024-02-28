544746c45c834559b2bef01f4c6994473dc616e2
Mariam <kelanimaryam1234@gmail.com>
2024-02-28 08:36:24 -0800

    15-binary_tree_is_full.c

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 
 size_t calculate_b_tree_height(const binary_tree_t *tree);
 
+int binary_tree_is_full(const binary_tree_t *tree);
+
 #endif /*_BINARY_TREES_H_*/
