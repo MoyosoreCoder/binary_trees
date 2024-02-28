e82e72a78414d8b6c88f2e89406c20481a57bc37
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-02-28 03:00:08 -0800

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		int left = 0, right = 0;
 
-	if (tree == NULL)
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (1 + (left_height > right_height ? left_height : right_height));
 }
